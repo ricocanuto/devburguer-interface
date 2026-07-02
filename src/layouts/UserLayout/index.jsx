a709c6659065729b4922d546b97ad12564837dcb
ricocanuto <ricardomoraescanuto@gmail.com>
2026-07-02 13:03:17 -0300

    set-localStorage

--- src/layouts/UserLayout/index.jsx
+++ src/layouts/UserLayout/index.jsx
@@ -1,17 +1,15 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { Footer, Header } from '../../components';
-import { useUser } from '../../hooks/UserContext';
 
 export function UserLayout() {
-    // 1. Busca os dados do usuário salvos no navegador (ajuste a chave se o nome for diferente)
-    const { userData } = useUser();
+    // Busca direto os dados salvos no navegador no momento do login
+    const userData = JSON.parse(localStorage.getItem('devburguer:userData'));
 
-    // 2. Se não houver dados de usuário, barra o acesso e joga direto para o Login
+    // Se não existir o usuário no localStorage, barra e redireciona
     if (!userData) {
         return <Navigate to="/login" replace />;
     }
 
-    // 3. Se estiver logado, exibe a estrutura normal do site com os componentes filhos
     return (
         <>
             <Header />
